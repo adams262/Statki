9141d12a43e19828f56b591aef7cddcfd1609862
DESKTOP-QAFVCTU\Eryk Przybyła <ded.vin@vp.pl>
2025-01-15 13:24:52 +0100

    main project folder

---
+++ Statki -EP&AS/Statki -EP&AS/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Statki__EP_AS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
